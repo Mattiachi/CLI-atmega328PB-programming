eab40ca827696187a424f280579e2d276627d3b6
Mattia~ <30445778+Mattiachi@users.noreply.github.com>
2022-04-25 22:57:32 +0200

    Create blink_uno.c

---
+++ blink_uno.c
@@ -0,0 +1,35 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+// This program blinks the builtin LED of the arduino Uno
+// Change in the Makefile the variables file_name (to match this file)
+// and the baud_rate (use 57600 for nano and 115200 for uno)
+// Author: M. Chiappalone
+// Date: April 2022
+
+int main(void) {
+	
+	// set PORTB5 as an output
+	// DDRD is Port D Data Direction Register. Output is enabled with a 1
+	DDRB |= (1 << DDB5);
+
+	// forever
+	while(1)
+	{
+
+		// set PORTD5 high
+		// PORTD is Port D data Register
+		PORTB |= (1 << PORTB5);
+
+		// wait
+		_delay_ms(1000);
+
+		// unset PORTB5
+		PORTB &= ~(1 << PORTB5);
+
+		// Wait again
+		_delay_ms(1000);
+
+	}
+}
